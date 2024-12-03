060571d79e2254a7c9a8e0fa67c87ba9f5899533
OcsCgn <Oscar.cocagne@free.fr>
2024-12-03 09:49:38 +0100

    commit

---
+++ app/Controllers/ProduitController.php
@@ -0,0 +1,21 @@
+<?php
+
+
+namespace App\Controllers;
+
+use CodeIgniter\Controller;
+use App\Models\ProduitModel;
+
+
+class ProduitController extends Controller{
+
+    public function __construct()
+	{
+		helper(filenames: ['form']);
+	}
+
+    public function index(){
+
+
+    }
+}
