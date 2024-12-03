12d941a61e5d3b620b195c65dd3f74b5f2c44302
Ledoux Gaétan <41428973+gaetanldx94@users.noreply.github.com>
2024-12-03 13:36:58 +0100

    Inscription

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -5,7 +5,7 @@ use CodeIgniter\Router\RouteCollection;
 /**
  * @var RouteCollection $routes
  */
-$routes->get('/', 'Home::index');
+$routes->get('/', 'AuthentificationController::index');
 
 $routes->get('blog', 'BlogController::index');
 $routes->get('blog/nouveau', 'BlogController::nouveau');
@@ -13,3 +13,6 @@ $routes->get('blog/nouveau', 'BlogController::nouveau');
 $routes->get('authentification', 'AuthentificationController::index');
 $routes->post('authentification/connexion', 'AuthentificationController::connexion');
 $routes->get('authentification/deconnexion', 'AuthentificationController::deconnexion');
+
+$routes->get('inscription', 'InscriptionController::index');
+$routes->post('inscription/creer', 'InscriptionController::creer');
---
+++ app/Controllers/InscriptionController.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Models\ClientModel;
+use CodeIgniter\Controller;
+
+class InscriptionController extends Controller
+{
+	public function index()
+	{
+		return view('Utilisateur/InscriptionVue');
+	}
+
+	public function creer()
+	{
+		$validation = \Config\Services::validation();
+
+		$validation->setRules([
+			'nom' => 'required|min_length[3]',
+			'prenom' => 'required|min_length[3]',
+			'email' => 'required|valid_email|is_unique[client.mail]',
+			'motdepasse' => 'required|min_length[6]',
+			'mobile' => 'required|numeric|min_length[10]',
+			'sexe' => 'required',
+			'age' => 'required|numeric|min_length[1]',
+			'taille' => 'required|numeric',
+			'poids_de_corps' => 'required|numeric',
+		]);
+
+		if (!$validation->withRequest($this->request)->run()) {
+			return view('InscriptionVue', [
+				'validation' => $validation
+			]);
+		}
+
+		$data = [
+			'nom' => $this->request->getPost('nom'),
+			'prenom' => $this->request->getPost('prenom'),
+			'email' => $this->request->getPost('email'),
+			'motdepasse' => password_hash($this->request->getPost('motdepasse'), PASSWORD_DEFAULT),
+			'mobile' => $this->request->getPost('mobile'),
+			'sexe' => $this->request->getPost('sexe'),
+			'age' => $this->request->getPost('age'),
+			'taille' => $this->request->getPost('taille'),
+			'poids_de_corps' => $this->request->getPost('poids_de_corps'),
+			'admin' => false,
+			'token' => bin2hex(random_bytes(16))
+		];
+
+		$clientModel = new ClientModel();
+		if ($clientModel->insert($data)) {
+			session()->setFlashdata('success', 'Inscription réussie, vous pouvez vous connecter.');
+			return redirect()->to('/authentification');
+		} else {
+			session()->setFlashdata('error', 'Une erreur est survenue. Veuillez réessayer.');
+			return redirect()->back()->withInput();
+		}
+	}
+}
