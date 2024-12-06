ba72398b1d88a62a10b70a4b187d1a2eac74bc82
Thibaut-Renaut <renautthibaut4@gmail.com>
2024-12-06 13:05:42 +0100

    améliotation front

--- app/Views/Accueil/HomeView.php
+++ app/Views/Accueil/HomeView.php
@@ -93,7 +93,7 @@
             <h1>Nos offres :</h1>
         </section>
 
-        <section class="wrapper">
+        <section class="wrapper offres">
             <div class="pricing-table gprice-single">
                 <div class="head">
                     <h4 class="title">Saiyan-90 JOURS</h4>
@@ -195,12 +195,204 @@
 
 
         </section>
+
+        <section class="why-not-you">
+            <div class="why-not-you-wrapper">
+                <div class="wrapper-content">
+                    <h1>Pourquoi pas <span class="h1-span" style="color : #F4E904;">toi</span> ?</h1>
+                    <p>Si toi aussi tu en as marre de te sentir mal dans ta peau, et que tu veux dire CIAO à tes
+                        complexes :</p>
+                    <h3>Rejoin les rangs !!!</h3>
+                    <a href="#" class="btn btn-why">Commencer maintenant !</a>
+                </div>
+            </div>
+        </section>
+
+        <section class="presentation">
+            <h1> <span class="h1-span" style="color : #F4E904;">Mindset</span> et <span class="h1-span"
+                    style="color : #F4E904;">skills</span></h1>
+            <!-- vidéo de présentation dans img/video.mp4 -->
+            <video controls class="video-presentation">
+                <source src="<?php echo base_url('assets/img/video.mp4'); ?>" type="video/mp4">
+                Your browser does not support the video tag.
+            </video>
+
+
+        </section>
+
+        <section class="caroussel">
+            <div class="caroussel-slide">
+                <img class="img-caroussel"
+                    src="/assets/img/caroussel-testimonial/caroussel-testimonial-1/partie-1/carousse1.png"
+                    style="width: 21%;" />
+                <img class="img-caroussel"
+                    src="/assets/img/caroussel-testimonial/caroussel-testimonial-1/partie-1/carousse4.png"
+                    style="width: 21%;" />
+                <img class="img-caroussel"
+                    src="/assets/img/caroussel-testimonial/caroussel-testimonial-1/partie-1/carousse5.png"
+                    style="width: 21%;" />
+                <img class="img-caroussel"
+                    src="/assets/img/caroussel-testimonial/caroussel-testimonial-1/partie-1/carousse6.png"
+                    style="width: 21%;" />
+            </div>
+            <div class="caroussel-slide">
+                <img class="img-caroussel"
+                    src="/assets/img/caroussel-testimonial/caroussel-testimonial-1/partie-1/carousse1.png"
+                    style="width: 21%;" />
+                <img class="img-caroussel"
+                    src="/assets/img/caroussel-testimonial/caroussel-testimonial-1/partie-1/carousse4.png"
+                    style="width: 21%;" />
+                <img class="img-caroussel"
+                    src="/assets/img/caroussel-testimonial/caroussel-testimonial-1/partie-1/carousse5.png"
+                    style="width: 21%;" />
+                <img class="img-caroussel"
+                    src="/assets/img/caroussel-testimonial/caroussel-testimonial-1/partie-1/carousse6.png"
+                    style="width: 21%;" />
+            </div>
+
+            <div class="opacity"></div>
+
+
+        </section>
+
+        <section class="caroussel-2">
+
+            <div class="caroussel-slide-2">
+                <img class="img-caroussel"
+                    src="/assets/img/caroussel-testimonial/caroussel-testimonial-1/partie-1/carousse5.png"
+                    style="width: 21%;" />
+                <img class="img-caroussel"
+                    src="/assets/img/caroussel-testimonial/caroussel-testimonial-1/partie-1/carousse6.png"
+                    style="width: 21%;" />
+
+                <img class="img-caroussel"
+                    src="/assets/img/caroussel-testimonial/caroussel-testimonial-1/partie-1/carousse4.png"
+                    style="width: 21%;" />
+                <img class="img-caroussel"
+                    src="/assets/img/caroussel-testimonial/caroussel-testimonial-1/partie-1/carousse1.png"
+                    style="width: 21%;" />
+            </div>
+
+            <div class="caroussel-slide-2">
+                <img class="img-caroussel"
+                    src="/assets/img/caroussel-testimonial/caroussel-testimonial-1/partie-1/carousse5.png"
+                    style="width: 21%;" />
+                <img class="img-caroussel"
+                    src="/assets/img/caroussel-testimonial/caroussel-testimonial-1/partie-1/carousse6.png"
+                    style="width: 21%;" />
+
+                <img class="img-caroussel"
+                    src="/assets/img/caroussel-testimonial/caroussel-testimonial-1/partie-1/carousse4.png"
+                    style="width: 21%;" />
+                <img class="img-caroussel"
+                    src="/assets/img/caroussel-testimonial/caroussel-testimonial-1/partie-1/carousse1.png"
+                    style="width: 21%;" />
+            </div>
+        </section>
+
+        <section id="content">
+            <div class="slide-wrapper">
+                <div class="slide">
+                    <div id="slide-1" class="testimonial">
+                        <blockquote>
+                            Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor.
+                        </blockquote>
+                        <div></div>
+                        <p>John Doe &mdash; Worcestershire, UK</p>
+                    </div>
+                    <div id="slide-2" class="testimonial">
+                        <blockquote>
+                            Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor.
+                        </blockquote>
+                        <div></div>
+                        <p>John Doe &mdash; Worcestershire, UK</p>
+                    </div>
+                    <div id="slide-3" class="testimonial">
+                        <blockquote>
+                            Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor.
+                        </blockquote>
+                        <div></div>
+                        <p>John Doe &mdash; Worcestershire, UK</p>
+                    </div>
+                    <div id="slide-4" class="testimonial">
+                        <blockquote>
+                            Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor.
+                        </blockquote>
+                        <div></div>
+                        <p>John Doe &mdash; Worcestershire, UK</p>
+                    </div>
+                </div>
+                <div class="slider-nav">
+                    <a href="#slide-1"></a>
+                    <a href="#slide-2"></a>
+                    <a href="#slide-3"></a>
+                    <a href="#slide-4"></a>
+                </div>
+            </div>
+        </section>
     </main>
-
     <footer>
-        <p>&copy; 2023 Deviens un Saiyan. Tous droits réservés.</p>
-    </footer>
+        <section class="section-infos">
+            <h1>Prêt à embarquer dans cette <span class="h1-span" style="color : #F4E904;">Aventure</span>?</h1>
+            <a href="#" class="btn btn-lato btn-infos">Plus d'informations</a>
+        </section>
+        <section class="footers">
 
+            <div class="footer-content">
+                <div class="footer-content-left">
+                    <div class="social-media">
+                        <a href="#"><i class="fa-brands fa-facebook"></i></a>
+                        <a href="#"><i class="fa-brands fa-instagram"></i></a>
+                        <a href="#"><i class="fa-brands fa-twitter"></i></a>
+                        <a href="#"><i class="fa-brands fa-youtube"></i></a>
+                    </div>
+                </div>
+                <div class="footer-content-right">
+                    <ul>
+                        <li><a href="#">Accueil</a></li>
+                        <li><a href="#">À propos</a></li>
+                        <li><a href="#">Programmes</a></li>
+                        <li><a href="#">Avant / Après</a></li>
+                        <li><a href="#">Blog</a></li>
+                        <li><a href="#">Actualité</a></li>
+                        <li><a href="#">Contact</a></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="footer-bottom">
+                <p>© 2024 Saiyan Coaching. Tous droits réservés.</p>
+            </div>
+        </section>
+    </footer>
 </body>
+
+<script>
+    document.addEventListener('DOMContentLoaded', () => {
+        const navDots = document.querySelectorAll('.slider-nav a');
+        const slides = document.querySelectorAll('.slide .testimonial');
+
+        navDots.forEach((dot, index) => {
+            dot.addEventListener('click', (e) => {
+                e.preventDefault(); // Empêche le comportement par défaut des ancres
+                slides[index].scrollIntoView({ behavior: 'smooth', block: 'nearest', inline: 'center' });
+            });
+        });
+
+        // Ajoute une classe active pour indiquer la slide en cours
+        const slideContainer = document.querySelector('.slide');
+        slideContainer.addEventListener('scroll', () => {
+            slides.forEach((slide, index) => {
+                const rect = slide.getBoundingClientRect();
+                const inView = rect.left >= 0 && rect.right <= window.innerWidth;
+
+                navDots.forEach((dot) => dot.classList.remove('active'));
+                if (inView) {
+                    navDots[index].classList.add('active');
+                }
+            });
+        });
+    });
+
+</script>
 
 </html>
