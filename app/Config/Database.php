817d0333c30e3e80e3f6079715ca9c2e6717c7c2
OcsCgn <Oscar.cocagne@free.fr>
2024-12-07 17:02:43 +0100

    commit

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -24,31 +24,32 @@ class Database extends Config
      *
      * @var array<string, mixed>
      */
-    public array $default = [
-        'DSN'          => 'Postgre://mm221005:Mohamad12345%@woody.iut.univ-lehavre.fr:5432',
-        'hostname'     => 'woody.iut.univ-lehavre.fr',
-        'username'     => 'mm221005',
-        'password'     => 'Mohamad12345%',
-        'database'     => 'mm221005',
-        'DBDriver'     => 'Postgre',
-        'DBPrefix'     => '',
-        'pConnect'     => false,
-        'DBDebug'      => true,
-        'charset'      => 'utf8',
-        'DBCollat'     => 'utf8_general_ci',
-        'swapPre'      => '',
-        'encrypt'      => false,
-        'compress'     => false,
-        'strictOn'     => false,
-        'failover'     => [],
-        'port'         => 5432,
-        'numberNative' => false,
-        'dateFormat'   => [
-            'date'     => 'Y-m-d',
-            'datetime' => 'Y-m-d H:i:s',
-            'time'     => 'H:i:s',
-        ],
-    ];
+   public array $default = [
+    'DSN'          => 'Postgre://localhost@localhost:5432/sayanCoaching',
+    'hostname'     => 'localhost',
+    'username'     => 'postgres', // Adaptez si vous utilisez un autre utilisateur
+    'password'     => '', // Remplacez par le mot de passe correct
+    'database'     => 'sayanCoaching',
+    'DBDriver'     => 'Postgre',
+    'DBPrefix'     => '',
+    'pConnect'     => false,
+    'DBDebug'      => true,
+    'charset'      => 'utf8',
+    'DBCollat'     => 'utf8_general_ci',
+    'swapPre'      => '',
+    'encrypt'      => false,
+    'compress'     => false,
+    'strictOn'     => false,
+    'failover'     => [],
+    'port'         => 5432,
+    'numberNative' => false,
+    'dateFormat'   => [
+        'date'     => 'Y-m-d',
+        'datetime' => 'Y-m-d H:i:s',
+        'time'     => 'H:i:s',
+    ],
+];
+
 
     //    /**
     //     * Sample database connection for SQLite3.
