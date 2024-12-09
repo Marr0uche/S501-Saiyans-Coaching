adb4628eafd62f0110ac43149dd0be1d90cc59de
OcsCgn <Oscar.cocagne@free.fr>
2024-12-09 12:43:16 +0100

    ViewInscription

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -24,22 +24,31 @@ class Database extends Config
      *
      * @var array<string, mixed>
      */
-  	public $default = [
-		'DSN'          => '',
-		'hostname'     => 'localhost',
-		'username'     => 'postgres', // ou votre nom d'utilisateur DB
-		'password'     => '',
-		'database'     => 'sayancoaching', // ou le nom de votre DB
-		'DBDriver'     => 'Postgre',
-		'DBPrefix'     => '',
-		'pConnect'     => false,
-		'DBDebug'      => true,
-		'charset'      => 'utf8',
-		'DBCollat'     => 'utf8_general_ci',
-		'port'         => 5432, // Assurez-vous que c'est bien le bon port
-		'encrypt'      => false,
-	];
-
+    public array $default = [
+        'DSN'          => 'Postgre://co221148:Galium140105@woody.iut.univ-lehavre.fr:5432',
+        'hostname'     => 'woody.iut.univ-lehavre.fr',
+        'username'     => 'co221148',
+        'password'     => 'Galium140105',
+        'database'     => 'co221148',
+        'DBDriver'     => 'Postgre',
+        'DBPrefix'     => '',
+        'pConnect'     => false,
+        'DBDebug'      => true,
+        'charset'      => 'utf8',
+        'DBCollat'     => 'utf8_general_ci',
+        'swapPre'      => '',
+        'encrypt'      => false,
+        'compress'     => false,
+        'strictOn'     => false,
+        'failover'     => [],
+        'port'         => 5432,
+        'numberNative' => false,
+        'dateFormat'   => [
+            'date'     => 'Y-m-d',
+            'datetime' => 'Y-m-d H:i:s',
+            'time'     => 'H:i:s',
+        ],
+    ];
 
     public function __construct()
     {
